379ee50f6d22124aeb1209546ab7c1e77b2edd57
konthis <kdkonstanti@csd.auth.gr>
2026-01-08 12:15:02 +0200

    comment out code, before publication

--- README.md
+++
@@ -1,1 +0,0 @@
-Code will be made available upon acceptance of the paper in ICPR.
--- models/KANmodel.py
+++
@@ -1,6 +0,0 @@
-from .KAN.LBFGS import *
-from .KAN.KANLayer import *
-from .KAN.MultKAN import *
-from .KAN.spline import *
-from .KAN.utils import *
-
